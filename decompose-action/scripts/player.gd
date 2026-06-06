25882d6700a0c165dc3e0209bc7944a63ca3b41e
Kenu4000 <komabuchi4000@gmail.com>
2026-06-06 18:52:03 +0900

    Let player movement continue under ability time scale

--- decompose-action/scripts/player.gd
+++ decompose-action/scripts/player.gd
@@ -3,21 +3,24 @@ extends CharacterBody2D
 @export var move_speed: float = 220.0
 @export var jump_velocity: float = -520.0
 @export var gravity: float = 1400.0
+@export var ability_friction: float = 120.0
 
 @onready var decompose_ui: Node = get_parent().get_node_or_null("DecomposeUI")
 
 func _physics_process(delta: float) -> void:
-	var locked: bool = false
+	var ability_active: bool = false
 	if decompose_ui != null and decompose_ui.has_method("is_active"):
-		locked = decompose_ui.is_active()
-
-	if locked:
-		return
+		ability_active = decompose_ui.is_active()
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	elif Input.is_action_just_pressed("jump"):
+	elif not ability_active and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_velocity
+
+	if ability_active:
+		velocity.x = move_toward(velocity.x, 0.0, ability_friction * delta)
+		move_and_slide()
+		return
 
 	var direction: float = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * move_speed
